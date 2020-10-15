130334ee2a4c26cdf4ea9bbd54510a5981523af3
mizuki-n-2 <mizukizhangze@gmail.com>
2020-10-16 00:03:02 +0900

    2020/10/15

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,6 @@ use App\Models\Message;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
