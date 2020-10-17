f6b745972e22afed876a0b84744c5abd01a35981
mizuki-n-2 <mizukizhangze@gmail.com>
2020-10-17 19:44:46 +0900

    2020/10/17

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,9 +26,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-
-        if (env('APP_ENV') === 'production') {
-            URL::forceScheme('https');
-        }
     }
 }
