d0dd52e3ecbf5b1d0c358fd7944fe0f1af2ac2f8
mizuki-n-2 <mizukizhangze@gmail.com>
2020-10-17 17:30:18 +0900

    2020/10/17

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        if ($this->app->environment() === 'production') {
+        if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
     }
